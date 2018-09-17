d743a3a0aeba7a5322cc0d5e8df46328ed7bc656
Caio Ramos Casimiro <caiorcasimiro@gmail.com>
2018-09-17 14:22:25 -0300

    implemented training and prediction

--- app/api.py
+++ app/api.py
@@ -1,18 +1,26 @@
 from celery import Celery
-from flask import Flask
 from config import logging
-from webargs import fields
-from webargs.flaskparser import use_kwargs
+from flask import abort
+from flask import Flask
+from flask import jsonify
+import numpy as np
+import pickle
+import redis
 from sklearn import linear_model
+import uuid
+from webargs import fields
+from webargs.flaskparser import use_args
+from webargs.flaskparser import use_kwargs
 
 
 app = Flask(__name__)
 app_queue = Celery(__name__, broker='redis://redis:6379/0')
+redis_instance = redis.Redis(host='redis')
 
 train_args = {
     'columns': fields.List(fields.Str(), required=True),
     'features': fields.List(fields.List(fields.Float()), required=True),
-    'classes': fields.List(fields.Str(), required=True)
+    'classes': fields.List(fields.Int(), required=True)
 }
 
 predict_args = {
@@ -24,21 +32,37 @@ predict_args = {
 
 
 @app_queue.task
-def training(columns, features, classes):
+def training(features, classes, columns, model_id):
     model = linear_model.LogisticRegression()
+    redis_instance.set(model_id.int, 'training')
     model.fit(features, classes)
-    pass
+    data = {'model': model, 'positions': columns}
+    redis_instance.set(model_id.int, pickle.dumps(data))
 
 
 @app.route('/models', methods=['POST'])
 @use_kwargs(train_args)
 def train(columns, features, classes):
-    return 'called post method'
+    logging.info('about to train')
+    model_id = uuid.uuid1()
+    training(features, classes, columns, model_id)
+    return jsonify({'model_id': str(model_id)}), 201
 
 
-@app.route('/models/<int:model_id>', methods=['GET'])
-@use_kwargs(predict_args)
-def predict(sepal_length, sepal_width, petal_length, petal_width, model_id):
-    logging.info(model_id)
-    logging.info(sepal_length)
-    return 'called get method'
+@app.route('/models/<uuid:model_id>', methods=['GET'])
+@use_args(predict_args)
+def predict(args, model_id):
+    id = uuid.UUID(str(model_id))
+    model_status = redis_instance.get(id.int)
+
+    if model_status is None:
+        abort(404)
+
+    if model_status == b'training':
+        return 'training'
+    else:
+        model_data = pickle.loads(model_status)
+        features = np.array([args[column] for column in model_data['positions']])
+        predicted_class = model_data['model'].predict(features.reshape(1, len(features)))
+        logging.info(predicted_class)
+        return jsonify({'predicted_class': int(predicted_class[0])})
